e6c6d725ca37283653886f870913409409e02e55
apatheia-virtue <111125094+apatheia-virtue@users.noreply.github.com>
2022-08-12 16:35:10 +0200

    Create google_drive.bat

---
+++ google_drive.bat
@@ -0,0 +1,22 @@
+@REM ---------- destination: google drive--------------
+@REM "/_out/.mp4"
+@REM "/_out/.aep"  
+@REM "/_out/.hip*" # except backup
+
+
+@REM ----------     destination: git     --------------
+@REM "/_out/.mp4"
+@REM "/_out/.aep"  
+@REM "/_out/.hip*" # except backup
+
+
+@REM ----------           folders        --------------
+@REM flip
+@REM kinefx
+@REM pyro
+@REM rbd
+@REM rnd_tools
+@REM solaris
+@REM unreal
+@REM vellum
+@REM vex
