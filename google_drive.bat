4be7bd8346b1bb59a6c39fe530d38f4f3d4c51c4
Jiri Miratsky <info@ikoon.cz>
2022-08-22 14:13:46 +0200

    Update google_drive.bat

--- google_drive.bat
+++ google_drive.bat
@@ -4,10 +4,12 @@
 @REM "/_out/.hip*" # except backup
 
 
+
 @REM ----------     destination: git     --------------
 @REM "/_out/.mp4"
 @REM "/_out/.aep"  
 @REM "/_out/.hip*" # except backup
+
 
 
 @REM ----------           folders        --------------
